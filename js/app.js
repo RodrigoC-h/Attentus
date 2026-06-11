6475560497bc813910244efbd38139f25a312577
RodrigoC-h <40250382@esmad.ipp.pt>
2026-06-11 11:30:52 +0100

    loja html + css

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import DefinicoesModel from "./js/model/DefinicoesModel.js";
-import DefinicoesView from "./js/view/DefinicoesView.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const definicoesModel = new DefinicoesModel();
-    new DefinicoesView(definicoesModel);
-});
-
